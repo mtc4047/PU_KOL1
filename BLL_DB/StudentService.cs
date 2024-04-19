9c9048790fa5cb4ef2438f119136fa917ce86b40
Student RM4-3073 12 <Student RM4-3073 12@RDS-LAB14A>
2024-04-19 17:33:39 +0200

    BLL_DB

---
+++ BLL_DB/StudentService.cs
@@ -0,0 +1,41 @@
+﻿using BLL.DTOModels;
+using BLL.ServiceInterfaces;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BLL_DB
+{
+    public class StudentService : IStudentService
+    {
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=PU_KOL1;Integrated Security=True;Connect Timeout=30;Encrypt=False;";
+        public void AddStudent(StudentRequestDTO student)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("AddStudent", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@Name", student.Imie);
+                command.Parameters.AddWithValue("@Price", student.Nazwisko);
+                command.Parameters.AddWithValue("@GroupId", student.IdGrupy);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteStudent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<StudentResponseDTO> GetStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateStudent(int id, StudentRequestDTO student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
