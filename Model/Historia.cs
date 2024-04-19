2b48d4752427586124fc7c7bcb5d05cdfa39068c
Student RM4-3073 12 <Student RM4-3073 12@RDS-LAB14A>
2024-04-19 15:43:07 +0200

    Dodaj pliki projektów.

---
+++ Model/Historia.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Historia
+    {
+        public int Id { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public int IdGrupy { get; set; }
+        public bool TypAkcji { get; set; }
+        public DateTime Data {  get; set; }
+
+    }
+}
---
+++ Model/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public int IdGrupy { get; set; }
+    }
+}
